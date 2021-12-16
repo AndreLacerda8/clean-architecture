21e204fbad91c509a15ac356deba3b519f5eaaa4
André Lacerda <andrlacerda180@gmail.com>
2021-12-16 10:43:22 -0300

    refactor: improve file names

--- src/presentation/controllers/login/login-controller.ts
+++ src/presentation/controllers/login/login-controller.ts
@@ -1,4 +1,4 @@
-import { HttpRequest, HttpResponse, Controller, Authentication, Validation } from './login-protocols'
+import { HttpRequest, HttpResponse, Controller, Authentication, Validation } from './login-controller-protocols'
 import { badRequest, ok, serverError, unauthorized } from '../../helpers/http/http-helper'
 
 export class LoginController implements Controller {
