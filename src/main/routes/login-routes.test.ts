1ce540013a305143cb8fd30612408700923ff9d2
André Lacerda <andrlacerda180@gmail.com>
2021-12-16 11:27:30 -0300

    test: login route returns 401 with invalid credentials

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -50,5 +50,15 @@ describe('Login Routes', () => {
         })
         .expect(200)
     })
+
+    it('Should return 401 on login', async () => {
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'andre@mail.com',
+          password: '123456'
+        })
+        .expect(401)
+    })
   })
 })
